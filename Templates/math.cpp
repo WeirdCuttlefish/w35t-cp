c66fb72e9661c4c323885d3dad22e27cd401c376
WeirdCuttlefish <WeirdCuttlefish@users.noreply.github.com>
2021-07-05 12:53:38 -0500

    Templates
    Added Math + Basic template

---
+++ Templates/math.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define um unordered_map
+#define endl "\n"
+
+typedef long long ll;
+typedef vector<int> vi;
+
+template <typename T>
+bool checkPrime(T n){
+    for(T i = 2; i < sqrt(n); i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+ 
+template <typename T>
+vector<T> primes(T n){
+    vector<T> ret;
+    bool prime[n+1];
+    T i = 2;
+    while(i * i <= n){
+        if(prime[i] == false){
+            ret.push_back(i);
+            for(int j = i * i; j <= n; j+=i)
+                prime[j] = true;
+        }
+        i++;
+    }
+    while(i < n+1){
+        if(!prime[i])
+            ret.push_back(i);
+
+        i++;
+    }
+    return ret;
+}
+
+int main(){
+    
+    return 0;
+}
---
+++ Templates/template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define um unordered_map
+#define endl "\n"
+
+typedef long long ll;
+typedef vector<int> vi;
+
+void solution(){
+    //-+**CODE HERE**+-
+    
+    //-+**CODE HERE**+-
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+    //TOUCHABLE
+    int testcases = 1;
+    //cin >> testcases;
+    for(int tn = 1; tn <= testcases; tn++){
+        //cout << "Case #" << tn << ": ";
+        solution();
+    }
+    return 0;
+}
